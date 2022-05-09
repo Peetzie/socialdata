a094f808dbd2747f518a871cdc22cafb066faf25
Peetzie <s174159@student.dtu.dk>
2022-05-09 10:12:57 +0200

    Added emojis, further contributions to locations

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -96,7 +96,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `${new Date().getFullYear()} Social Data and Visualisations,  Built with Docusaurus.`,
       },
     }),
 };
